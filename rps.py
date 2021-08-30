fa9cd4a4c4c7325b0a6513ae7962225c7b283195
Dan <danjtoms@gmail.com>
2021-08-30 22:28:36 +0100

    initial upload of file

---
+++ rps.py
@@ -0,0 +1,50 @@
+#import modules
+import random
+
+
+#set list of plays and winning pairs
+plays = ["ROCK","PAPER","SCISSORS"]
+beats = [("ROCK","SCISSORS"),("PAPER","ROCK"),("SCISSORS")]
+
+
+#result functions
+#winner
+def winner():
+    print("You WON! Congratulations!")
+
+#loser
+def loser():
+    print("You LOST! Better luck next time!")
+
+
+#assign random play to computer
+play_comp = random.choice(plays)
+
+
+#force player to choose a valid option
+while True:
+
+    #player chooses play
+    play_player = input("ROCK, PAPER or SCISSORS? ").upper()
+
+    #check player input is valid. If not, try again.
+    if play_player in plays:
+        break
+    else:
+        print(play_player + ". Invalid selection. Try again.")
+
+
+#announce plays
+print("You play " + play_player)
+print("Computer plays " + play_comp)
+
+
+#determine winner
+if play_player == play_comp:
+    print("It's a DRAW!")
+else:
+    #computer chose rock
+    if (play_player,play_comp) in beats:
+        winner()
+    else:
+        loser()
