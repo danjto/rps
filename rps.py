e4e1d71ec170a75ebc4a6b711ea52d1d7c173e70
Dan <danjtoms@gmail.com>
2021-08-31 07:10:09 +0100

    program now asks to play again. main() added

--- rps.py
+++ rps.py
@@ -9,35 +9,42 @@ plays = ["ROCK","PAPER","SCISSORS"]
 beats = [("ROCK","SCISSORS"),("PAPER","ROCK"),("SCISSORS")]
 
 
-#assign random play to computer
-play_comp = random.choice(plays)
+def main():
+    #assign random play to computer
+    play_comp = random.choice(plays)
 
 
-#force player to choose a valid option
+    #force player to choose a valid option
+    while True:
+
+        #player chooses play
+        play_player = input("ROCK, PAPER or SCISSORS? ").upper()
+
+        #check player input is valid. If not, try again.
+        if play_player in plays:
+            break
+        else:
+            print(f"{play_player}. Invalid selection. Try again.")
+
+
+    #announce plays
+    print(f"You play {play_player}")
+    print(f"Computer plays {play_comp}")
+
+
+    #determine winner
+    if play_player == play_comp:
+        print("It's a DRAW!")
+    else:
+        #if winning pair
+        if (play_player,play_comp) in beats:
+            print(winner)
+
+        else:
+            print(loser)
+
+
 while True:
-
-    #player chooses play
-    play_player = input("ROCK, PAPER or SCISSORS? ").upper()
-
-    #check player input is valid. If not, try again.
-    if play_player in plays:
+    main()
+    if input("Play again? (Y/N) ").strip().upper() != "Y":
         break
-    else:
-        print(f"{play_player}. Invalid selection. Try again.")
-
-
-#announce plays
-print(f"You play {play_player}")
-print(f"Computer plays {play_comp}")
-
-
-#determine winner
-if play_player == play_comp:
-    print("It's a DRAW!")
-else:
-    #computer chose rock
-    if (play_player,play_comp) in beats:
-        print(winner)
-
-    else:
-        print(loser)
